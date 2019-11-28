cef68fa87b8a42f6eafeee740bd8587d8182f3e0
gongwei <gongwei@hikcreate.com>
2019-11-28 17:02:44 +0800

    利用python加walle完成Release apk的打包、签名、加固、打渠道包全流程

---
+++ buildsrc/src/main/groovy/com/gongva/plugin/config/PluginConfig.groovy
@@ -0,0 +1,15 @@
+
+package com.gongva.plugin.config
+
+/**
+ * 不同的工程需要添加的配置，都是相对路径
+ *
+ * @author wangtao55* @date 2019/9/4
+ * @mail wangtao55@hikcreate.com
+ */
+class PluginConfig{
+    //build文件夹路径
+    static def buildPath =  File.separator + "build"+ File.separator
+    //release包产生的路径
+    static def releaseApkPath = buildPath + "outputs" + File.separator + "apk" + File.separator + "release"
+}
---
+++ buildsrc/src/main/groovy/com/gongva/plugin/extension/ChannelExtension.groovy
@@ -0,0 +1,10 @@
+
+package com.gongva.plugin.extension
+/**
+ * 涉及到的扩展属性
+ */
+class ChannelExtension {
+    String path360 //360jar包位置
+    String sdkBuildToolPath //buildToolPath
+    String appName //编译工程的名称，用于寻找中间过程apk
+}
